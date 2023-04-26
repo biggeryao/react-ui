3e90c1915a0e0d2333111d0a17e8b999ebfeffb8
biggeryao <62866598+biggeryao@users.noreply.github.com>
2023-04-26 17:43:03 +0800

    Button文档

--- src/components/Button/button.stories.tsx
+++ src/components/Button/button.stories.tsx
@@ -4,7 +4,7 @@ import {ComponentMeta, ComponentStory, composeStory} from "@storybook/react";
 import exp from "constants";
 
 const buttonMete: ComponentMeta<typeof Button> = {
-    title: 'button',
+    title: 'Button',
     component: Button,
     tags: ['autodocs'],
 
@@ -17,9 +17,21 @@ export const Default: ComponentStory<typeof Button> = (args) => {
 }
 Default.storyName = '默认按钮'
 
-export const ButtonWithSize: ComponentStory<typeof Button> = () => {
+export const ButtonWithSize: ComponentStory<typeof Button> = (args) => {
     return (
-        <Button size="lg"></Button>
+        <div>
+            <Button {...args} size="lg">不同尺寸的按钮</Button>
+            <Button style={{marginLeft:'10px'}} {...args} size="sm">不同尺寸的按钮</Button>
+        </div>
     )
 }
 ButtonWithSize.storyName = '不同尺寸的按钮'
+
+export const ButtonWithType:ComponentStory<typeof Button>=(args)=>{
+    return (<div>
+        <Button {...args} btnType="primary">Primary button</Button>
+        <Button {...args} btnType="danger">Danger button</Button>
+        <Button {...args} btnType="link" href="baidu.com">Link button</Button>
+    </div>)
+}
+ButtonWithType.storyName='不同类型的按钮'
--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -8,8 +8,11 @@ export type ButtonType = 'primary' | 'default' | 'danger' | 'link'
 
 interface BaseButtonProps {
     className?: string,
+    /**设置 Button 的禁用 */
     disabled?: boolean,
+    /**设置Button的尺寸 */
     size?: ButtonSize,
+    /**设置 Button 的类型**/
     btnType?: ButtonType,
     children: React.ReactNode,
     href?: string
@@ -21,6 +24,14 @@ type AnchorButtonProps = BaseButtonProps & React.AnchorHTMLAttributes<HTMLElemen
 
 export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>
 
+/**
+ * 页面中最常用的的按钮元素，适合于完成特定的交互，支持 HTML button 和 a 链接 的所有属性
+ * ### 引用方法
+ *
+ * ```javascript
+ * import { Button } from 'vikingship'
+ * ```
+ */
 export const Button: React.FC<ButtonProps> = (props) => {
     const {
         btnType,
