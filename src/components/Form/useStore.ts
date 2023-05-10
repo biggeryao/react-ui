0a0a9c8892671c2be7e0365379bc4274966d5943
biggeryao <62866598+biggeryao@users.noreply.github.com>
2023-05-10 15:38:05 +0800

    发布npm

--- src/components/Form/useStore.ts
+++ src/components/Form/useStore.ts
@@ -56,11 +56,28 @@ function fieldsReducer(state: FieldsState, action: FieldAction): FieldsState {
     }
 }
 
-function useStore() {
+function useStore(initialValues?:Record<string, any>) {
     const [form, setForm] = useState({isValid: true,isSubmitting:false,errors:{}})
     const [fields, dispatch] = useReducer(fieldsReducer, {})
     const getFieldValue=(key:string)=>{
         return fields[key]&&fields[key].value
+    }
+    const getFieldsValue=()=>{
+        return mapValues(fields,item=>item.value)
+    }
+    const setFieldValue=(name:string,value:any)=>{
+        if(fields[name]){
+            dispatch({type:'updateValue',name,value})
+        }
+    }
+    const resetFieldValue=()=>{
+        if(initialValues){
+                each(initialValues,(value,name)=>{
+                    if(fields[name]){
+                        dispatch({type:'updateValue',name,value})
+                    }
+                })
+        }
     }
     const transformRules = (rules: CustomRule[]) => {
         return rules.map(rule => {
@@ -136,7 +153,10 @@ function useStore() {
         form,
         validateField,
         getFieldValue,
-        validateAllFields
+        validateAllFields,
+        resetFieldValue,
+        setFieldValue,
+        getFieldsValue
     }
 }
 
